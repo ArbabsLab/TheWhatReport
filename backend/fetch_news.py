f24eb3fb6bfe00a42bb33d1ce45bd2464db43475
Arbab <arbabhusain200@gmail.com>
2025-01-30 23:33:30 -0500

    Added fetch_news_source

--- backend/fetch_news.py
+++ backend/fetch_news.py
@@ -6,8 +6,15 @@ load_dotenv()
 
 api_key = os.getenv('NEWSAPI_KEY')
 
-def fetch_news(query="Politics",sort_by = "popularity",key=api_key):
+def fetch_news_keyword(query="Politics",sort_by = "popularity",key=api_key):
     url = f"https://newsapi.org/v2/everything?q={query}&sortBy={sort_by}&apiKey={key}"
+    
+    req = requests.get(url)
+
+    return req.json
+
+def fetch_news_source(source="us", key=api_key):
+    url = f"https://newsapi.org/v2/top-headlines?country={source}&apiKey={key}"
     
     req = requests.get(url)
 
