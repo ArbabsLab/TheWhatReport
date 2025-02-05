e40c54c5f4695137a7578dc5ae884654ef052900
Arbab Husain <arbabhusain200@gmail.com>
2025-02-04 20:55:06 -0500

    Added CORS

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 import fetch_news
 import mod_data
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
